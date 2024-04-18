6c099e42fc94455fc4c12433c8c17c379e22c89c
Stephen O Driscoll <stephenodriscoll98@gmail.com>
2024-04-18 01:49:07 +0100

    Removed RangeNoOverlap class due to issues with operator overloading

--- GenericUtilsTests/RangeTests.cpp
+++ GenericUtilsTests/RangeTests.cpp
@@ -57,43 +57,28 @@ TEST_F(RangeTests, TestRangeIsEqual)
     ASSERT_FALSE(sizeRange4 == sizeRange3);
 }
 
-TEST_F(RangeTests, TestRangeNoOverlapIsOverlapping)
+TEST_F(RangeTests, TestRangeIsOverlapping)
 {
-    Generic::SizeRangeNoOverlap sizeRange1{ 0, 0 };
-    Generic::SizeRangeNoOverlap sizeRange2{ 0, 0 };
-    Generic::SizeRangeNoOverlap sizeRange3{ 1, 5 };
-    Generic::SizeRangeNoOverlap sizeRange4{ 2, 6 };
+    Generic::SizeRange sizeRange1{ 0, 0 };
+    Generic::SizeRange sizeRange2{ 0, 0 };
+    Generic::SizeRange sizeRange3{ 1, 5 };
+    Generic::SizeRange sizeRange4{ 2, 6 };
 
     ASSERT_FALSE(sizeRange1.isOverlapping(sizeRange2));
     ASSERT_FALSE(sizeRange2.isOverlapping(sizeRange1));
     ASSERT_TRUE(sizeRange3.isOverlapping(sizeRange4));
     ASSERT_TRUE(sizeRange4.isOverlapping(sizeRange3));
-
-    ASSERT_FALSE(sizeRange1 == sizeRange2);
-    ASSERT_FALSE(sizeRange2 == sizeRange1);
-    ASSERT_TRUE(sizeRange3 == sizeRange4);
-    ASSERT_TRUE(sizeRange4 == sizeRange3);
 }
 
-TEST_F(RangeTests, TestRangeNoOverlapIsLess)
+TEST_F(RangeTests, TestRangeIsLessNoOverlap)
 {
-    Generic::SizeRangeNoOverlap sizeRange1{ 0, 0 };
-    Generic::SizeRangeNoOverlap sizeRange2{ 0, 0 };
-    Generic::SizeRangeNoOverlap sizeRange3{ 1, 5 };
-    Generic::SizeRangeNoOverlap sizeRange4{ 2, 6 };
+    Generic::SizeRange sizeRange1{ 0, 0 };
+    Generic::SizeRange sizeRange2{ 0, 0 };
+    Generic::SizeRange sizeRange3{ 1, 5 };
+    Generic::SizeRange sizeRange4{ 2, 6 };
 
     ASSERT_TRUE(sizeRange1.isLessNoOverlap(sizeRange2));
     ASSERT_TRUE(sizeRange2.isLessNoOverlap(sizeRange1));
     ASSERT_FALSE(sizeRange3.isLessNoOverlap(sizeRange4));
     ASSERT_FALSE(sizeRange4.isLessNoOverlap(sizeRange3));
-
-    ASSERT_TRUE(sizeRange1 < sizeRange2);
-    ASSERT_TRUE(sizeRange2 < sizeRange1);
-    ASSERT_FALSE(sizeRange3 < sizeRange4);
-    ASSERT_FALSE(sizeRange4 < sizeRange3);
-
-    ASSERT_TRUE(sizeRange1 > sizeRange2);
-    ASSERT_TRUE(sizeRange2 > sizeRange1);
-    ASSERT_FALSE(sizeRange3 > sizeRange4);
-    ASSERT_FALSE(sizeRange4 > sizeRange3);
 }
--- include/Generic/Range.hpp
+++ include/Generic/Range.hpp
@@ -19,14 +19,14 @@ namespace Generic
 
     public:
         Range(const Type& begin, const Size& size = {}) :
-            m_begin{ begin },
-            m_size{ size } {}
+            m_begin { begin },
+            m_size  { size } {}
 
         Range(const Range& other) :
-            m_begin{ other.m_begin },
-            m_size{ other.m_size } {}
+            m_begin { other.m_begin },
+            m_size  { other.m_size } {}
 
-        virtual ~Range() {}
+        ~Range() {}
 
         void begin(const Type& begin)   { m_begin = begin; }
         void size(const Size& size)     { m_size = size; }
@@ -68,55 +68,23 @@ namespace Generic
             return (!isLessNoOverlap(other) && !other.isLessNoOverlap(*this));
         }
 
-        virtual bool operator<(const Range& other) const
+        bool operator<(const Range& other) const
         {
             return isLess(other);
         }
 
-        virtual bool operator>(const Range& other) const
+        bool operator>(const Range& other) const
         {
             return other.isLess(*this);
         }
 
-        virtual bool operator==(const Range& other) const
+        bool operator==(const Range& other) const
         {
             return isEqual(other);
-        }
-    };
-
-    template<class Type, class Size = Type>
-    class RangeNoOverlap : public Range<Type, Size>
-    {
-    public:
-        RangeNoOverlap(const Type& begin, const Size& size = {}) :
-            Range<Type, Size>(begin, size) {}
-
-        RangeNoOverlap(const Range<Type, Size>& other) :
-            Range<Type, Size>(other) {}
-
-        ~RangeNoOverlap() override {}
-
-        bool operator<(const Range<Type, Size>& other) const override
-        {
-            return Range<Type, Size>::isLessNoOverlap(other);
-        }
-
-        bool operator>(const Range<Type, Size>& other) const override
-        {
-            return other.isLessNoOverlap(*this);
-        }
-
-        bool operator==(const Range<Type, Size>& other) const override
-        {
-            return Range<Type, Size>::isOverlapping(other);
         }
     };
 
     typedef Range<int>                  IntRange;
     typedef Range<std::size_t>          SizeRange;
     typedef Range<void*, std::size_t>   PVoidRange;
-
-    typedef RangeNoOverlap<int>                 IntRangeNoOverlap;
-    typedef RangeNoOverlap<std::size_t>         SizeRangeNoOverlap;
-    typedef RangeNoOverlap<void*, std::size_t>  PVoidRangeNoOverlap;
 }
