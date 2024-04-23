5460789b1df7c864a598baa00cb9457f6f7f3c85
Stephen O Driscoll <32990628+Stephen-ODriscoll@users.noreply.github.com>
2024-04-23 23:46:15 +0100

    Fixed gcc build and improved build scripts

---
+++ GenerateDebug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+cmake .. -D CMAKE_CXX_STANDARD=20 -D CMAKE_BUILD_TYPE=Debug
+cd ..
--- generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-cmake .. -D CMAKE_CXX_STANDARD=20
-cd ..
