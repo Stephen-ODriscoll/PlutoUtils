8b091d8183e93c61d8990f2059967dbe8acc5f32
Stephen O Driscoll <stephenodriscoll98@gmail.com>
2024-05-28 01:58:07 +0100

    Prefer using char* argv[] over char** argv

--- GenericUtilsTests/Main.cpp
+++ GenericUtilsTests/Main.cpp
@@ -8,7 +8,7 @@
 #define GTEST_LANG_CXX11 1
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
