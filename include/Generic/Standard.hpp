df4ef8c897376673df7881faf56b9b8d7888bb71
Stephen O Driscoll <stephenodriscoll98@gmail.com>
2024-06-13 19:27:50 +0100

    Added sections for C++ 11 and C++ 14 for completeness

--- include/Generic/Standard.hpp
+++ include/Generic/Standard.hpp
@@ -8,40 +8,29 @@
 #pragma once
 
 #include <algorithm>
-#include <array>
-#include <atomic>
 #include <bitset>
 #include <cassert>
 #include <cctype>
 #include <cerrno>
-#include <cfenv>
 #include <cfloat>
-#include <chrono>
-#include <cinttypes>
 #include <climits>
 #include <clocale>
 #include <cmath>
 #include <complex>
-#include <condition_variable>
 #include <csetjmp>
 #include <csignal>
 #include <cstdarg>
 #include <cstddef>
-#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <cuchar>
 #include <cwchar>
 #include <cwctype>
 #include <deque>
 #include <exception>
-#include <forward_list>
 #include <fstream>
 #include <functional>
-#include <future>
-#include <initializer_list>
 #include <iomanip>
 #include <ios>
 #include <iosfwd>
@@ -53,33 +42,52 @@
 #include <locale>
 #include <map>
 #include <memory>
-#include <mutex>
 #include <new>
 #include <numeric>
 #include <ostream>
 #include <queue>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
 #include <set>
-#include <shared_mutex>
 #include <sstream>
 #include <stack>
 #include <stdexcept>
 #include <streambuf>
 #include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// C++ 11
+#if (defined(_MSVC_LANG) && _MSVC_LANG > 199711L) || (defined(__cplusplus) && __cplusplus > 199711L)
+#include <array>
+#include <atomic>
+#include <cfenv>
+#include <chrono>
+#include <cinttypes>
+#include <condition_variable>
+#include <cstdint>
+#include <cuchar>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
 #include <system_error>
 #include <thread>
 #include <tuple>
 #include <type_traits>
 #include <typeindex>
-#include <typeinfo>
 #include <unordered_map>
 #include <unordered_set>
-#include <utility>
-#include <valarray>
-#include <vector>
+#endif
+
+// C++ 14
+#if (defined(_MSVC_LANG) && _MSVC_LANG > 201103L) || (defined(__cplusplus) && __cplusplus > 201103L)
+#include <shared_mutex>
+#endif
 
 // C++ 17
 #if (defined(_MSVC_LANG) && _MSVC_LANG > 201402L) || (defined(__cplusplus) && __cplusplus > 201402L)
