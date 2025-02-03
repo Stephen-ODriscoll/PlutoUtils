95bb2c6937e6627d3062a306fe158c272d1c0877
Stephen O Driscoll <stephenodriscoll98@gmail.com>
2025-02-03 16:49:24 +0000

    Corrected copyright year

--- include/pluto/value_utils.hpp
+++ include/pluto/value_utils.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2024 Stephen O Driscoll
+* Copyright (c) 2025 Stephen O Driscoll
 *
 * Distributed under the MIT License (See accompanying file LICENSE)
 * Official repository: https://github.com/Stephen-ODriscoll/PlutoUtils
